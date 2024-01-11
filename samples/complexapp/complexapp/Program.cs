fdabbc7839f88096a6a33ef20cf8e16c969d3920
Charles Oxyer <46503080+charlesoxyer@users.noreply.github.com>
2024-01-11 14:54:17 -0800

    Update Program.cs

--- samples/complexapp/complexapp/Program.cs
+++ samples/complexapp/complexapp/Program.cs
@@ -1,9 +1,13 @@
-﻿using libbar;
+using libbar;
 using libfoo;
 using static System.Console;
 
 string initialString = StringLibrary.GetString();
 string reversedString = StringUtils.ReverseString(initialString);
 
+// Spoof Credential for Testing Secret Scanning Tools
+string spoofApiKey = "ABCD1234-FAKE-API-KEY-5678EFGH"; // Fake API Key
+
 WriteLine($"string: {initialString}");
 WriteLine($"reversed: {reversedString}");
+WriteLine($"Spoof API Key: {spoofApiKey}"); // Displaying spoof credential (for testing only)
